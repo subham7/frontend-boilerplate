295eab219b8e45c0969456d338caf7a0e798195f
unknown <ankitsocial16@gmail.com>
2019-04-25 11:54:13 +0530

    Chnaged url

--- src/api/ROOTURL.js
+++ src/api/ROOTURL.js
@@ -1,4 +1,4 @@
 let ROOTURL
 // Server Url
 // export default (ROOTURL = "http://koyofea.com/api")
-export default (ROOTURL = "http://13.126.27.108")
+export default (ROOTURL = "http://13.233.149.249")

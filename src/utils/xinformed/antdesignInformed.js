3273668fbdda1a899b256f1c688d7facf2b9da25
Ankit_Choudhary <ankitsocial16@gmail.com>
2019-06-15 16:24:00 +0530

    purchase review functionality

--- src/utils/xinformed/antdesignInformed.js
+++ src/utils/xinformed/antdesignInformed.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Form, asField, Select, Option } from "informed"
+import { Form, asField, Select, Option , useFormState } from "informed"
 import { Input, Radio, Checkbox, DatePicker } from "antd"
 // import 'antd/dist/antd.css';
 
@@ -161,4 +161,4 @@ const Sel = props => {
   )
 }
 
-export { Form, Text_type1, Check, O, Tarea, Sel, R, RG, Date }
+export { Form, Text_type1, Check, O, Tarea, Sel, R, RG, Date ,useFormState}

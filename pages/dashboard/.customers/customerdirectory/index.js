8db821e7332e79a49a46b1c7aeb3a1d2793715ea
Pranay Agarwal <pranayag15@gmail.com>
2019-06-17 23:22:48 +0530

    customer directory and hsn to tax

--- pages/dashboard/.customers/customerdirectory/index.js
+++ pages/dashboard/.customers/customerdirectory/index.js
@@ -7,7 +7,7 @@ import {
   customerData
 } from "./customerDirectory.data"
 
-import { allUsers } from "../../../../src/reduxHelper"
+import { businessUsers, customerReceipts } from "../../../../src/reduxHelper"
 import ROOTURL from "../../../../src/api/ROOTURL"
 import axios from "axios"
 
@@ -16,13 +16,17 @@ class App extends Component {
     super(props)
     this.state = {
       customerDetails: [],
+      receiptData: [],
       usersTableData: [],
       filteredTableData: []
     }
   }
 
   componentDidMount() {
-    this.props.getUsers().then(res => {
+    let businessID = this.props.business.response.data[0].businessID
+    console.log(businessID)
+    this.props.getUsers(businessID).then(res => {
+      console.log(res)
       this.setState({ filteredTableData: res, usersTableData: res })
     })
   }
@@ -30,7 +34,13 @@ class App extends Component {
   handleClick = userID => {
     console.log(userID)
     axios.get(`${ROOTURL}/users?_where=(userID,eq,${userID})`).then(res => {
+      console.log(res)
       this.setState({ customerDetails: res.data })
+      return this.props.getReceipts(userID)
+    })
+    .then(res => {
+      console.log(res)
+      this.setState({receiptData: res})
     })
   }
 
@@ -44,14 +54,14 @@ class App extends Component {
   }
 
   render() {
-    if (this.props.allUsers.response)
+    if (this.props.businessUsers.response)
       return (
         <CustomerDirectory
           listData={this.state.filteredTableData}
           customerData={this.state.customerDetails}
           actionData={["abcd", "efgh"]}
           date="Today 12-12-2019"
-          receiptCardData={receiptCardData}
+          receiptCardData={this.state.receiptData}
           onClick={this.handleClick}
           onSearch={value => this.handleSearch(value)}
         />
@@ -61,11 +71,13 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  allUsers: state.allUsers
+  business: state.businesses,
+  businessUsers: state.businessUsers
 })
 
 const mapDispatchToProps = dispatch => ({
-  getUsers: () => dispatch(allUsers.action())
+  getUsers: (businessID) => dispatch(businessUsers.action(businessID)),
+  getReceipts: (customerID) => dispatch(customerReceipts.action(customerID))
 })
 
 export default connect(

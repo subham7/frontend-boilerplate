52815c1255d1b26b165395afd7f54a188a7d2ece
Pranay Agarwal <pranayag15@gmail.com>
2019-10-22 14:09:14 +0530

    removed build

--- out/_next/static/chunks/styles.f50619a2f2c6c05d490c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["ad9d"],{"35vW":function(n,o,c){},"8nl2":function(n,o,c){},FAfY:function(n,o,c){},GPnO:function(n,o,c){},"Ibq+":function(n,o,c){},"K/SL":function(n,o,c){},Lsha:function(n,o,c){},M9t7:function(n,o,c){},Svq7:function(n,o,c){},Wfs9:function(n,o,c){},a6CB:function(n,o,c){},gnA7:function(n,o,c){},hEgN:function(n,o,c){}}]);

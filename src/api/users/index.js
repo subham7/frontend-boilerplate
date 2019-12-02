5cdf936c13579ed22f76c3163b63e8c4126e867e
Pranay Agarwal <pranayag15@gmail.com>
2019-12-02 23:11:08 +0530

    unique business customers

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -6,6 +6,6 @@ export const user = (userID) => axios.get(`${ROOTURL}/users/${userID}`)
 
 export const allUsers = () => axios.get(`${ROOTURL}/users`)
 
-export const uniqueCustomers = (businessID) => axios.get(`${ROOTURL}/uniquecustomers`)
+export const uniqueCustomers = (businessID) => axios.get(`${ROOTURL}/uniquecustomers?business=${businessID}`)
 
 export const customerReceipts = (customerID) => axios.get(`${ROOTURL}/users/${customerID}/receipts`)

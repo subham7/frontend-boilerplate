90b24ce71a62472b4c3055d3f125e4cef56cef6f
Subham Sengupta <subham.sengupta7@gmail.com>
2020-04-06 02:21:14 +0530

    og:image change

--- pages/wrapper.js
+++ pages/wrapper.js
@@ -80,7 +80,7 @@ export default function init(WrappedComponent) {
             <meta property="og:title" content="My Goto" />
             <meta
               property="og:image"
-              content="https://koyofea.s3.ap-south-1.amazonaws.com/icon.jpg"
+              content="https://mygoto.in/static/icons/icon.jpg"
             />
             <meta
               property="og:description"
